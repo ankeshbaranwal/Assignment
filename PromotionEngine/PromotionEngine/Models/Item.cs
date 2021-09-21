c302f4afb7026cb4e1aedb21afbb7c91a86eecb1
ankeshbaranwal <ankesh.baranwal@gmail.com>
2021-09-21 13:15:37 +0100

    Model Classes

---
+++ PromotionEngine/PromotionEngine/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Models
+{
+    class Item
+    {
+        public char SKUId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ PromotionEngine/PromotionEngine/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+        public char SKUId { get; set; }
+        public int Price { get; set; }
+        public int AvailableQuantity { get; set; }
+    }
+}
