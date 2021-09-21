ec90dceae598fd234156b52e7a995919c4a18e99
ankeshbaranwal <ankesh.baranwal@gmail.com>
2021-09-21 17:40:18 +0100

    Using Dependency Injection feature and Refactoring

--- PromotionEngine/PromotionEngine/Program.cs
+++ PromotionEngine/PromotionEngine/Program.cs
@@ -1,4 +1,5 @@
-﻿using PromotionEngine.Models;
+﻿using Microsoft.Extensions.DependencyInjection;
+using PromotionEngine.Models;
 using PromotionEngine.Services;
 using System;
 
@@ -8,36 +9,43 @@ namespace PromotionEngine
     {
         static void Main(string[] args)
         {
-            ProductService productService = new ProductService();
-            CartService cartService = InitializeCartServiceWithCart("Scenario B", productService);
-            PromotionService promotionService = new PromotionService();
-            PromotionEngineService engineService = new PromotionEngineService(promotionService, productService);
+
+            ServiceCollection services = new ServiceCollection();
+
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IPromotionService, PromotionService>();
+            services.AddScoped<IPromotionEngineService, PromotionEngineService>();
+            services.AddTransient<ICartService, CartService>();
+            ServiceProvider provider = services.BuildServiceProvider();
+
+            var engineService = provider.GetService<IPromotionEngineService>();
+            //Scenario A
+            var cartService = provider.GetService<ICartService>();
+            cartService.AddToCart('A', 1);
+            cartService.AddToCart('B', 1);
+            cartService.AddToCart('C', 1);
             Cart cart = engineService.CheckOut("Promotion1", cartService.GetCart());
+            Console.WriteLine($"Total Amount for Scenario A after promotion is : {cart.TotalAmount}");
+
+            //Scenario B
+            cartService = provider.GetService<ICartService>();
+            cartService.AddToCart('A', 5);
+            cartService.AddToCart('B', 5);
+            cartService.AddToCart('C', 1);
+            cart = engineService.CheckOut("Promotion1", cartService.GetCart());
+            Console.WriteLine($"Total Amount for Scenario B after promotion is : {cart.TotalAmount}");
+
+            //Scenario B
+            cartService = provider.GetService<ICartService>();
+            cartService.AddToCart('A', 3);
+            cartService.AddToCart('B', 5);
+            cartService.AddToCart('C', 1);
+            cartService.AddToCart('D', 1);
+            cart = engineService.CheckOut("Promotion1", cartService.GetCart());
+            Console.WriteLine($"Total Amount for Scenario C after promotion is : {cart.TotalAmount}");
+
+            Console.ReadLine();
         }
 
-        private static CartService InitializeCartServiceWithCart(string scenario, ProductService productService)
-        {
-            CartService cartService = new CartService(productService);
-            if (scenario.Equals("Scenario A", StringComparison.OrdinalIgnoreCase))
-            {
-                cartService.AddToCart('A', 1);
-                //cartService.AddToCart('B', 1);
-                //cartService.AddToCart('C', 1);
-            }
-            else if (scenario.Equals("Scenario B", StringComparison.OrdinalIgnoreCase))
-            {
-                cartService.AddToCart('A', 5);
-                cartService.AddToCart('B', 5);
-                cartService.AddToCart('C', 1);
-            }
-            else if (scenario.Equals("Scenario C", StringComparison.OrdinalIgnoreCase))
-            {
-                cartService.AddToCart('A', 3);
-                cartService.AddToCart('B', 5);
-                cartService.AddToCart('C', 1);
-                cartService.AddToCart('D', 1);
-            }
-            return cartService;
-        }
     }
 }
