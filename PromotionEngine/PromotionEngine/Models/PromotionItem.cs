456e4aa4f9f62d0258817d7a6c41f4da0524f83e
ankeshbaranwal <ankesh.baranwal@gmail.com>
2021-09-21 13:35:01 +0100

    Adding model for Promotion

---
+++ PromotionEngine/PromotionEngine/Models/PromotionItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Models
+{
+    class PromotionItem
+    {
+        public List<Item> Items { get; set; }
+        public int Value { get; set; }
+    }
+}
